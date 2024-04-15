eecf0fbb7ee2cb33c5643f17ae8428a8d627a963
Min Zhao <42870954+balaenazhao@users.noreply.github.com>
2024-04-15 14:29:04 -0400

    Update extract_annual_raster.R

--- Environmental Data/extract_annual_raster.R
+++ Environmental Data/extract_annual_raster.R
@@ -3,10 +3,10 @@ library(dplyr)
 library(ggplot2)
 library(patchwork)
 
-# run below scripts in bash
-#cat tas_*/* > tas.combined.csv
-#grep "Muscicapa sibirica" tas.combined.csv > Muscicapa_sibirica_tas.csv
-#grep "Copsychus saularis" tas.combined.csv > Copsychus_saularis_tas.csv
+### get coordianates from tas data file
+allbirds_tas<- read.csv("tas.combined.csv",header=T,sep=',')
+allbirds_pr<- read.csv("pr.combined.csv",header=T,sep=',')
+bird_coor <- data.frame(allbirds_tas[,"lon"],allbirds_tas[,"lat"])
 
 ########################################################################
 ##################### exatract from annual variables ###################
@@ -14,48 +14,55 @@ library(patchwork)
 temp_raster<- raster("CHELSA/CHELSA_bio1_1981-2010_V.2.1_temperature.tif")
 rain_raster<- raster("CHELSA/CHELSA_bio12_1981-2010_V.2.1_precipitation.tif")
 
-#### resident bird Copsychus_saularis
-Copsychus_saularis<- read.csv("Copsychus_saularis_tas.csv",header=T,sep=',')
-bird_coor <- data.frame(Copsychus_saularis[,"lon"],Copsychus_saularis[,"lat"])
 temp <- extract(temp_raster,bird_coor,method='bilinear',df=TRUE)
 temp$temperature <- temp$CHELSA_bio1_1981.2010_V.2.1_temperature * 0.1 - 273.5
 rain <- extract(rain_raster,bird_coor,method='bilinear',df=TRUE)
 rain$precipitation <- rain$CHELSA_bio12_1981.2010_V.2.1_precipitation * 0.1
-bird_Data<- data.frame(bird_coor,temp$temperature,rain$precipitation)
-write.csv(bird_Data,file = "Copsychus_saularis_Annual_Data.csv")
+bird_annual_Data<- data.frame(allbirds_tas[,"species"],bird_coor,temp$temperature,rain$precipitation)
 
-#### migratory bird Muscicapa_sibirica
-Muscicapa_sibirica<- read.csv("Muscicapa_sibirica_tas.csv",header=T,sep=',')
-bird_coor <- data.frame(Muscicapa_sibirica[,"lon"],Muscicapa_sibirica[,"lat"])
-temp <- extract(temp_raster,bird_coor,method='bilinear',df=TRUE)
-temp$temperature <- temp$CHELSA_bio1_1981.2010_V.2.1_temperature * 0.1 - 273.5
-rain <- extract(rain_raster,bird_coor,method='bilinear',df=TRUE)
-rain$precipitation <- rain$CHELSA_bio12_1981.2010_V.2.1_precipitation * 0.1
-bird_Data<- data.frame(bird_coor,temp$temperature,rain$precipitation)
-write.csv(bird_Data,file = "Muscicapa_sibirica_Annual_Data.csv")
+#### calculate mean values for all birds
+temp_mean <- aggregate(bird_annual_Data[,5],list(bird_annual_Data$species),mean,na.rm=TRUE, na.action=na.pass)
+precip_mean <- aggregate(bird_annual_Data[,6],list(bird_annual_Data$species),mean,na.rm=TRUE, na.action=na.pass)
+colnames(temp_mean)[1] <- "species"
+colnames(temp_mean)[2] <- "temperature"
+colnames(precip_mean)[1] <- "species"
+colnames(precip_mean)[2] <- "precipitation"
+write.csv(temp_mean, "Temperature_annual_mean.csv", row.names = FALSE)
+write.csv(precip_mean, "Precipitation_annual_mean.csv", row.names = FALSE)
 
-##################### use temperature as example ###################
-#### plot boxplots
-data1<- read.csv(file="Copsychus_saularis_Annual_Data.csv",header=T)
-data2<- read.csv(file="Copsychus_saularis_tas.csv",header=T)
+####################### testing on two species ####################
+
+#### resident bird Copsychus saularis
+data1<- bird_annual_Data[bird_annual_Data$species == "Copsychus saularis",]
+data2<- allbirds_tas[allbirds_tas$species == "Copsychus saularis",]
 data1$type<- "annual"
 data2$type <- "monthly"
-dat1<- select(data1,temp.temperature,type)
-colnames(dat1)[1]<- "temp"
-dat2<-select(data2,temp,type)
-plot_dat<- rbind(dat1,dat2)
-p1<- ggplot(plot_dat, aes(x=type, y=temp)) + geom_boxplot()
+dat1<- select(data1,temp.temperature,rain.precipitation,type)
+colnames(dat1)[1]<- "tas"
+colnames(dat1)[2]<- "pr"
+dat2<-select(data2,tas,pr,type)
+plot_dat1<- rbind(dat1,dat2)
+p1<- ggplot(plot_dat, aes(x=type, y=tas)) + geom_boxplot()
+p2<- ggplot(plot_dat, aes(x=type, y=pr)) + geom_boxplot()
 
-data3<- read.csv(file="Muscicapa_sibirica_Annual_Data.csv",header=T)
-data4<- read.csv(file="Muscicapa_sibirica_tas.csv",header=T)
+#### migratory bird Muscicapa sibirica
+data3<- bird_annual_Data[bird_annual_Data$species == "Muscicapa sibirica",]
+data4<- allbirds_tas[allbirds_tas$species == "Muscicapa sibirica",]
 data3$type<- "annual"
 data4$type <- "monthly"
-dat3<- select(data3,temp.temperature,type)
-colnames(dat3)[1]<- "temp"
-dat4<-select(data4,temp,type)
+dat3<- select(data3,temp.temperature,rain.precipitation,type)
+colnames(dat3)[1]<- "tas"
+colnames(dat4)[2]<- "pr"
+dat4<-select(data4,tas,pr,type)
 plot_dat2<- rbind(dat3,dat4)
-p2<- ggplot(plot_dat2, aes(x=type, y=temp)) + geom_boxplot()
+p3<- ggplot(plot_dat2, aes(x=type, y=tas)) + geom_boxplot()
+p4<- ggplot(plot_dat2, aes(x=type, y=pr)) + geom_boxplot()
 
-pdf("./boxplot.pdf", width = 8, height = 8)
-p1 + p2
-dev.off()
+#### plot mean values comparison across all species
+tas_mean<- read.csv("Tas_Mean_Data.csv",header=T,sep=',')
+df<- merge(temp_mean,tas_mean,by="species")
+p5<- ggplot(df, aes(x = species)) + 
+  geom_line(aes(x = species, y = temperature), color="blue", size=2) + 
+  geom_line(aes(x = species, y = temperature), color="red", size=2)
+
+
